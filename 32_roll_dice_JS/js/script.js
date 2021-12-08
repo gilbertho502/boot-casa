44dd0b2bf4b7056392804f3119d9e74ea6c63db7
Gilberto Vicente <2965479210101@ingenieria.usac.edu.gt>
2021-12-08 17:56:54 -0600

    Ejercicio con dados

---
+++ 32_roll_dice_JS/js/script.js
@@ -0,0 +1,55 @@
+let random = Math.random(); // 0.01 ... 0.99
+let suerte = random * 6; //  numero aletorio (1.??-5.??) -> 1 .. 5
+let dado2 = parseInt(suerte) + 1; //( 1 ... 5) + 1
+
+//(0,1) * 10 = 0 - 9.?? +1
+// let dado = parseInt(Math.random() * 10) + 1;
+// (0,1) * 10 = 9.74 = 10
+let dado = Math.round(Math.random() * 6);
+
+let obj1 = document.getElementById("salida");
+let opcion
+
+opcion = prompt("Seguir jugando?")
+if (opcion == "si"){
+    if (dado > 3) {
+        obj1.innerHTML = ` Has ganado, ha salido  el número ${dado} `;
+        obj1.innerHTML = `<br><br>`
+      
+        
+        if (dado ==4){
+          obj1.innerHTML = `<br><img src="images/cara4.jpg" alt=""> ${dado}`
+        }
+        if (dado ==5){
+      
+          obj1.innerHTML = `<br><img src="images/cara5.jpg" alt=""> ${dado}`
+        }
+        if (dado ==6){
+      
+          obj1.innerHTML = `<br><img src="images/cara6.jpg" alt=""> ${dado}`
+        }
+      } 
+      
+      else {
+        obj1.innerHTML = ` Lo siento, ha salido el número ${dado} `;
+        obj1.innerHTML = `<br><br>`
+        if (dado ==1){
+          obj1.innerHTML = `<br><img src="images/cara1.jpg" alt=""> ${dado}`
+        }
+        if (dado ==2){
+      
+          obj1.innerHTML = `<br><img src="images/cara2.jpg" alt=""> ${dado}`
+        }
+        if (dado ==3){
+      
+          obj1.innerHTML = `<br><img src="images/cara3.jpg" alt=""> ${dado}`
+        }
+      }
+
+}
+else{
+    obj1.innerHTML = `El juego ha terminado!`
+}
+
+
+
