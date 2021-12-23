a75549e0e05d4d64b82dfe020e4b5930b94decb5
Gilberto Vicente <vicenteyoc15@gmail.com>
2021-12-22 18:23:59 -0600

    solucion a ejercicios en js

---
+++ 34_solucion_ejercicio8/js/script.js
@@ -0,0 +1,21 @@
+/* Curso de historia */
+// Constantes
+const CREDITOS = 3, P1 = 0.30, P2 = 0.3, PF= 0.4
+
+//Funciones
+
+
+//MAIN
+
+let nota_p1 =4.1, nota_p2=4, nota_ef= 4
+
+let promedio = 0 
+let producto = 0
+
+nota_p1 = nota_p1*P1
+nota_p2 = nota_p2*P2
+nota_ef = nota_ef*PF
+promedio = (nota_p1+ nota_p1+nota_ef)
+producto = promedio * CREDITOS 
+
+console.log("Acumulado de historia es: ", producto)
