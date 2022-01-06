523d95362a8da1f41b9618f8b10e4c4cb1fa20de
Gilberto Vicente <vicenteyoc15@gmail.com>
2022-01-05 21:14:39 -0600

    ejercicio 1

---
+++ 39_ejercicios_DOM/Ejercicio 1/js/script.js
@@ -0,0 +1,15 @@
+const boton = document.querySelector("#boton");
+const color = document.querySelector("#texto");
+const normal = document.querySelector("#regresar");
+
+function texto(){
+    color.style.color = 'green';
+}
+function regresar(){
+    color.style.color = 'black';
+}
+
+
+boton.addEventListener('click', texto);
+normal.addEventListener('click', regresar);
+
