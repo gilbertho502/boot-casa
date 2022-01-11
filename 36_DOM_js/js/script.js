8cfc0fe3c1ba2ee765d1b3417873b32cd752ffb2
Gilberto Vicente <vicenteyoc15@gmail.com>
2022-01-10 20:46:24 -0600

    uso de DOM

---
+++ 36_DOM_js/js/script.js
@@ -0,0 +1,22 @@
+console.log("hola mundo")
+
+const page1 = document.getElementById("page");
+console.log(page1)
+
+const list = document.getElementsByClassName("box")
+console.log(list)
+console.log(list[0])
+
+
+const opts = document.getElementsByName("genero")
+console.log(opts)
+
+const divs = document.getElementsByTagName("div")
+console.log(divs)
+
+const page = document.querySelector("#page")
+console.log(page)
+
+const info = document.querySelector(".main .info")
+console.log(info)
+
